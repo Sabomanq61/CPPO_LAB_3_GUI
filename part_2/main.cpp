8b79cbd8199c2c84061e7da505d501b7de0f77bd
Sabomanq61 <saba.ukubaev@mail.ru>
2020-06-25 20:30:23 +0700

    чистака main.cpp от тестов фич

--- part_2/main.cpp
+++ part_2/main.cpp
@@ -14,20 +14,4 @@ int main(int argc, char *argv[])
     w.show();
 
     return a.exec();
-
-    //QFileInfo t("D:\dev\CPPO\CPPO_LAB_3_GUI\part_2\test\test3");
-
-    //StrategyFolder explore;
-    //std::shared_ptr<ExplorerStrategy::FileList> pointer = explore.Explore("../part_2/test/test3");
-    /*StrategyType expType;
-    std::shared_ptr<ExplorerStrategy::FileList> pointer = expType.Explore("../part_2/test/test3");
-*/
-    //for(auto& it : *pointer)
-    //{
-    //    QTextStream(stdout) << it.name_ << ' ' << it.size_ << ' ' << it.percent_ << endl <<flush;
-    //}
-/*
-    QString t = QString::number(float(456)/10);
-    QTextStream(stdout) << t;*/
-    return 0;
 }
