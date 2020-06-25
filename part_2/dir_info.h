5a9757d390d5c404cc8b05ec2b55a458c9e0a0a4
Sabomanq61 <saba.ukubaev@mail.ru>
2020-06-25 20:26:37 +0700

    Добавление знака '%' при формировании структуры

--- part_2/dir_info.h
+++ part_2/dir_info.h
@@ -7,7 +7,7 @@ struct FileInfo
 public:
     //
     FileInfo(const QString& name = "", const int64_t& size = 0, const float& percent = 0)
-        : name_(name), size_(QString::number(size)), percent_(QString::number(percent))
+        : name_(name), size_(QString::number(size)), percent_(QString::number(percent, 'g', 4) + '%')
     {}
 
     QString name_;
