56be3b67cbf6bdda8acd7464e60f693d1bd810f4
Sabomanq61 <saba.ukubaev@mail.ru>
2020-06-28 17:01:35 +0700

    Удаление ненужных заголовков

--- part_2/main.cpp
+++ part_2/main.cpp
@@ -1,10 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <iostream>
-#include "dir_info.h"
-#include "folder_strategy.h"
-#include "type_strategy.h"
-#include <QTextStream>
+
+
 
 int main(int argc, char *argv[])
 {
